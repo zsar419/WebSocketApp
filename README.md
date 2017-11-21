73b93ee202c0f85f9013ba7a3f07db4d27efce65
zsar419 <zsar419@aucklanduni.ac.nz>
2017-11-21 18:53:11 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Websocket Web App
+A web socket which allows client-server communication and broadcast functionality
+- Web-socket client link: ws://
+
+## Instructions ##
+- npm install
+- npm start
+- Connect to: ws://localhost:3000/
+    - using web-socket client such as Smart Websocket Client (chrome extension)
+        - https://chrome.google.com/webstore/detail/smart-websocket-client/omalebghpgejjiaoknljcfmglgbpocdp
+- To send messages to all clients type: "broadcast: <your-message>"
+
+## Web Sockets ##
+- Web socket lib: https://github.com/websockets/ws
+- A communications protocol that provides a full-duplex communication channels over a single TCP connection established between a web browser (client) and a web server (this take place through the “classic HTTP mechanism” of handshaking, implemented using request/response headers)
+- This allows the server to send content to the browser without being called by the client, pushing data through the opened connection, defining ws and wss as URI schemes used respectively for unencrypted and encrypted connections
+- Since using http under the hood, WS can use authorization headers
---
+++ src/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+
+// Initialize a simple http server
+const server = http.createServer(app);
+
+// Initialize WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+    // Ping messages to identify broken connections
+    ws.isAlive = true;
+    ws.on('pong', () => { ws.isAlive = true; });
+
+    // Connection is up - ready to recieve messages
+    ws.on('message', (message) => {
+
+        // Log the received message -> send it back to the client
+        console.info(`received: ${message}`);
+        ws.send(`You sent -> ${message}`);
+    });
+
+    ws.on('message', (message) => {
+
+        // Connection is up - ready to recieve messages
+        console.info(`received: ${message}`);
+
+        // Regex to identify brodcast messages
+        const broadcastRegex = /^broadcast\:/;
+        if (broadcastRegex.test(message)) {
+            message = message.replace(broadcastRegex, '');
+
+            //send back the message to the other clients
+            wss.clients
+                .forEach(client => {
+                    if (client != ws) {
+                        client.send(`Broadcast: ${message}`);
+                    }
+                });
+
+        } else {
+            // Normal client-server messages
+            ws.send(`You sent -> ${message}`);
+        }
+    });
+
+    // Send feedback to incoming client initial connection   
+    ws.send('Connected to WebSocket server...');
+});
+
+/**
+ * The link between the server and the client can be interrupted in a way that both are unaware of the broken state of the connection
+ * To avoid this situation we use ping messages to check if the client is still responsive
+ */
+setInterval(() => {
+    wss.clients.forEach((ws) => {
+        if (!ws.isAlive) {
+            return ws.terminate();
+        }
+        
+        ws.isAlive = false;
+        ws.ping(null, false, true);
+    });
+}, 2000);  // Check at 10s intervals
+
+// Start server
+server.listen(process.env.PORT || 3000, () => {
+    console.info(`Server started on port ${server.address().port}...`);
+});
